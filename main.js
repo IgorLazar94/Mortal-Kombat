7a963de5eafa6732fb84b7703b89db2ebbdf5b9d
IgorLazar94 <81099964+IgorLazar94@users.noreply.github.com>
2021-05-31 21:14:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// alert('Fight...')
+
+
+// const firstName = 'Skorpion';
+// console.log(firstName);
+let numbers = 4;
+console.log(typeof numbers);
+
+let string = 'span';
+console.log(typeof string);
+
+let boolean = true;
+console.log(typeof boolean);
+
+const infini = '3' / 0;
+console.log(infini);
+
+const nan = NaN;
+console.log(nan);
+
+let undef = undefined;
+console.log(typeof undef);
+
+let bigin = BigInt(3);
+console.log(typeof bigin);
+
+
+
+
+
